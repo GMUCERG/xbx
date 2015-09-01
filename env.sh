b8e2b253e30fedd68a1d2dc918d70ac5629ea164
John Pham <jhnphm@gmail.com>
2015-08-31 21:37:28 -0400

    Update instructions, install script

---
+++ env.sh
@@ -0,0 +1,1 @@
+export PATH=~/.opt/xbx/bin:~/.opt/xbx/arm-tiva_c-eabi/bin:$PATH
--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
-XBX_WORKDIR=$(pwd)
-set -o v
+export XBX_WORKDIR=$(pwd)
+export PREFIX=~/.opt/xbx
+export PATH=~/.opt/xbx/bin:$PATH
+
+set -v
 sudo apt-get -y install git gcc make autoconf automake subversion gperf \
     libtool bison flex texinfo gawk libncurses-dev libusb-1.0-0-dev g++ \
     zlib1g-dev libexpat-dev python-dev python3-sqlalchemy sqlitebrowser
@@ -9,5 +12,27 @@ sudo apt-get -y install git gcc make autoconf automake subversion gperf \
 mkdir -p $XBX_WORKDIR/tools/tarballs
 
 git submodule init
-git submodule update --recursive
+pushd xbh
+git submodule init
+popd
+pushd xbs_xbd
+git submodule init
+popd
 
+git submodule update --recursive --remote
+
+pushd $XBX_WORKDIR/tools
+git clone https://github.com/crosstool-ng/crosstool-ng.git -b crosstool-ng-1.21.0
+git clone https://github.com/jhnphm/tiva-c_xtools.git
+git clone https://github.com/jhnphm/openocd.git
+
+pushd crosstool-ng
+./bootstrap && ./configure --prefix=$PREFIX && make -j && make -j install
+popd
+pushd tiva-c_xtools
+ct-ng build
+popd
+pushd openocd
+./bootstrap && ./configure --prefix=$PREFIX && make -j && make -j install
+popd
+popd
