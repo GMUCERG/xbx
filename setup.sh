14cd22b1871448b316fe2912f71552f75e9b42c2
Brian Hession <hessionb@gmail.com>
2019-06-15 09:08:04 -0400

    Added KEM/Sign support

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ export PATH=~/.opt/xbx/bin:$PATH
 set -v
 sudo apt-get -y install git gcc make autoconf automake subversion gperf \
     libtool bison flex texinfo gawk libncurses-dev libusb-1.0-0-dev g++ \
-    zlib1g-dev libexpat-dev python-dev python3-sqlalchemy sqlitebrowser
+    zlib1g-dev libexpat-dev python-dev python3-sqlalchemy sqlitebrowser \
+	openocd
 
 # Make working directory and tools and tarballs subdirectory
 mkdir -p $XBX_WORKDIR/tools/tarballs
@@ -18,21 +19,34 @@ popd
 pushd xbs_xbd
 git submodule init
 popd
+pushd SUPERCOP
+git submodule init
+popd
 
 git submodule update --init --recursive
 
-pushd $XBX_WORKDIR/tools
-git clone https://github.com/crosstool-ng/crosstool-ng.git -b crosstool-ng-1.21.0
-git clone https://github.com/jhnphm/tiva-c_xtools.git
-git clone https://github.com/jhnphm/openocd.git
+pushd xbh
+git checkout master
+popd
+pushd xbs_xbd
+git checkout master
+popd
+pushd SUPERCOP
+git checkout master
+popd
 
-pushd crosstool-ng
-./bootstrap && ./configure --prefix=$PREFIX && make -j && make -j install
-popd
-pushd tiva-c_xtools
-ct-ng build
-popd
-pushd openocd
-./bootstrap && ./configure --prefix=$PREFIX && make -j && make -j install
-popd
-popd
+#pushd $XBX_WORKDIR/tools
+#git clone https://github.com/crosstool-ng/crosstool-ng.git -b crosstool-ng-1.21.0
+#git clone https://github.com/jhnphm/tiva-c_xtools.git
+#git clone https://github.com/jhnphm/openocd.git
+#
+#pushd crosstool-ng
+#./bootstrap && ./configure --prefix=$PREFIX && make -j && make -j install
+#popd
+#pushd tiva-c_xtools
+#ct-ng build
+#popd
+#pushd openocd
+#./bootstrap && ./configure --prefix=$PREFIX && make -j && make -j install
+#popd
+#popd
